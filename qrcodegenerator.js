500ddd3bb1991897c52c421af54b3fb5fe71efb1
TechnoZombie <124879219+TechnoZombie@users.noreply.github.com>
2024-02-11 17:53:33 +0000

    added async function to download generated image

--- qrcodegenerator.js
+++ qrcodegenerator.js
@@ -1,69 +1,75 @@
-const apiThrowAway = 'http://api.qrserver.com/v1/create-qr-code/,' +
-    '?data=google.com,' +
-    '&size=150x150,' +
-    '&color=0f0,' +
-    '&bgcolor=0-0-0,' +
-    '&qzone=1,' +
-    '&format=png';
-
+const apiThrowAway = 'http://api.qrserver.com/v1/create-qr-code/';
 $(document).ready(function () {
     const generateButton = $('#buttonGenerate');
 
     generateButton.click(function () {
-        generateQR();
+        generateQr();
     });
 
     const resetButton = $('#resetButton');
     resetButton.click(function () {
-        $("#inputUrl").val('');
-        $("#sizeSelection").val('Default Size');
-        $('.generatedQRcode').empty();
+        clearFields();
     });
 
+    $(document).on('click', '#downloadButton', function () {
+        downloadImage();
+    });
 
+    function clearFields() {
+        $("#inputUrl").val('');
+        $("#sizeSelection").val('Default Size');
+        $("#bgColorSelect").val('Background Color');
+        $("#codeColorSelect").val('Code Color');
+        $('.generatedQRcode').empty();
+    }
 
 });
 
-function generateQR() {
-    let qrCodeUrl = 'http://api.qrserver.com/v1/create-qr-code/'
+function generateQr() {
+    let qrCodeUrl = apiThrowAway;
     let data = $('#inputUrl').val();
     let size = $('#sizeSelection').val();
     let bgColor = colorDecoder($('#bgColorSelect').val());
     let codeColor = colorDecoder($('#codeColorSelect').val());
 
-    console.log(size)
-
     qrCodeUrl += '?data=' + data;
     qrCodeUrl += '&size=' + size;
     qrCodeUrl += '&bgcolor=' + bgColor;
     qrCodeUrl += '&color=' + codeColor;
-
     qrCodeUrl += '&qzone=1';
     qrCodeUrl += '&format=png';
 
-    console.log(qrCodeUrl);
-
     injectQrCode(data, qrCodeUrl);
-
 }
 
 function injectQrCode(data, imgUrl) {
     if (data === "") {
-        $('.generatedQRcode').append('URL is empty')
+        $('.generatedQRcode').append('URL is empty');
     } else {
         $('.generatedQRcode').empty();
         $('.generatedQRcode').append('<img src=' + imgUrl + '>');
+        $('.generatedQRcode').append('<br><button type="button" id="downloadButton">Download</button>');
     }
 }
 
-function colorDecoder(color){
-    switch (color){
+function colorDecoder(color) {
+    switch (color) {
         case 'Black':
-        return '0-0-0';
-
+            return '0-0-0';
         case 'Matrix Green':
             return '0f0';
-
     }
 }
 
+async function downloadImage() {
+    let imgUrl = $('.generatedQRcode img').attr('src');
+    let response = await fetch(imgUrl);
+    let blobImage = await response.blob();
+    let url = window.URL.createObjectURL(blobImage);
+    let a = document.createElement('a');
+    a.href = url;
+    a.download = 'qr_code.png';
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+}
